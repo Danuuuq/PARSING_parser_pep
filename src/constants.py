d1bd831d5c505804b9bda267dd9f405085d9f513
Daniil Tyunkov <daniil@tyunkov.ru>
2025-01-03 21:52:08 +0300

    start pep parser

---
+++ src/constants.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+PEP_DOC_URL = 'https://peps.python.org/'
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+LOG_FORMAT = '"%(asctime)s - [%(levelname)s] - %(message)s"'
+DT_FORMAT = '%d.%m.%Y %H:%M:%S'
